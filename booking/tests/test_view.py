99003824b27b9081a0730763fa6aa44737988e3f
Patrick Kimber <code@pkimber.net>
2014-01-13 12:02:46 +0000

    test create

--- booking/tests/test_view.py
+++ booking/tests/test_view.py
@@ -1,4 +1,7 @@
-from datetime import datetime
+from datetime import (
+    datetime,
+    timedelta,
+)
 
 from django.core.urlresolvers import reverse
 from django.test import TestCase
@@ -17,20 +20,34 @@ class TestView(TestCase):
     def setUp(self):
         default_scenario_login()
         default_scenario_booking()
-
-    def test_list(self):
         staff = get_user_staff()
+        # update simple content
         self.assertTrue(
             self.client.login(username=staff.username, password=STAFF)
         )
+
+    def test_create(self):
+        today = datetime.today().date()
+        from_date = today + timedelta(days=2)
+        to_date = today + timedelta(days=5)
+        response = self.client.post(
+            reverse('booking.create'),
+            dict(
+                from_date=from_date,
+                to_date=to_date,
+                title='Hatherleigh',
+            )
+        )
+        self.assertEqual(response.status_code, 302)
+        # check booking
+        b = Booking.objects.get(title='Hatherleigh')
+        self.assertEqual('Hatherleigh', b.title)
+
+    def test_list(self):
         response = self.client.get(reverse('booking.list'))
         self.assertEqual(response.status_code, 200)
 
-    def test_month(self):
-        staff = get_user_staff()
-        self.assertTrue(
-            self.client.login(username=staff.username, password=STAFF)
-        )
+    def test_list_month(self):
         today = datetime.today().date()
         response = self.client.get(
             reverse(
