2372c460d23f3acbc98e1133b0618255a15eb629
Patrick Kimber <code@pkimber.net>
2014-01-14 15:33:22 +0000

    init migrations

---
+++ booking/migrations/0001_initial.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Booking'
+        db.create_table(u'booking_booking', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+            ('from_date', self.gf('django.db.models.fields.DateField')()),
+            ('to_date', self.gf('django.db.models.fields.DateField')()),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('description', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+        ))
+        db.send_create_signal(u'booking', ['Booking'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Booking'
+        db.delete_table(u'booking_booking')
+
+
+    models = {
+        u'booking.booking': {
+            'Meta': {'ordering': "['from_date']", 'object_name': 'Booking'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'from_date': ('django.db.models.fields.DateField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'to_date': ('django.db.models.fields.DateField', [], {})
+        }
+    }
+
+    complete_apps = ['booking']
