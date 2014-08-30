ae422a4f408541baf8881248f8175a06e900eca8
Patrick Kimber <code@pkimber.net>
2014-08-30 18:21:49 +0100

    rename 'project.home.user' to 'project.dash'

--- example/urls.py
+++ example/urls.py
@@ -31,7 +31,7 @@ urlpatterns = patterns(
         ),
     url(r'^home/user/$',
         view=RedirectView.as_view(url=reverse_lazy('project.home')),
-        name='project.home.user'
+        name='project.dash'
         ),
 )
 
