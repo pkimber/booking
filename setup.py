29d409c9b549cddf0731a73adf0fab368a3cb7ba
Patrick Kimber <code@pkimber.net>
2014-11-14 20:21:49 +0100

    version 0.0.10

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
             'templates/booking/*.*',
         ],
     },
-    version='0.0.09',
+    version='0.0.10',
     description='booking',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
